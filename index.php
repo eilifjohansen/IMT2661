9809ca858bc6eb0b132bdeda9e798d2d9207c777
eilifjohansen <eilifjohansen@outlook.com>
2015-06-03 23:32:05 +0200

    Version 0.1

--- index.php
+++ index.php
@@ -7,12 +7,22 @@
 </head>
 <body>
 	<header>
-		
+		<img src="img/yogblack_transparentmby.png" alt="Logo">
 	</header>
 
-	<a href="quiz.php">Start quiz</a>
-
-
+	<section>
+		<a href="quiz.php">
+			<button type="button" id="start">Start quiz</button>
+		</a>
+		<br>
+		<a href="rankings.php">
+			<button type="button" id="rankings">Rankings</button>
+		</a>
+		<br>
+		<a href="settings.php">
+			<button type="button" id="settings">Settings</button>
+		</a>
+	</section>
 
 	<script src="js/script.js"></script>
 </body>
